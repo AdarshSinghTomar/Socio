dfd19f6142789b242cff321b5f857eb7dc54a12c
AdarshSinghTomar <veracryptic@gmail.com>
2022-10-01 00:31:19 +0530

    Setting up package.json

---
+++ Routers/routehandler.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const routes=express.Router;
+
+console.log('Middleware is running perfectly');
+module.exports(routes);
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const app=express();
-const port=8000;
-
+const port=8080;
+console.log ("server is running")
 
 
 app.listen(port,function(err){
